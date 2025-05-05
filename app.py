8fd1a2b4242f5ef6229227d87a8d1fb2875780fa
KARTHICKJK1208 <160461673+KARTHICKJK1208@users.noreply.github.com>
2025-05-05 14:40:22 +0530

    front-end link

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def recommend(movie):
         return movie_list
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'movie-recommender-app/build'), static_url_path='/')
-CORS(app, resources={r"/api/*": {"origins": ["https://movies-recommendation-1tns.onrender.com", "https://movies-recommendation-frontend.onrender.com", "http://localhost:3000"]}})
+CORS(app, resources={r"/api/*": {"origins": ["https://movies-recommendation-1tns.onrender.com", "https://movies-recommendation-1-m13t.onrender.com", "http://localhost:3000"]}})
 
 @app.route('/api/movies', methods=['GET'])
 def movies():
