cf9bfb8045cf85bde84731c21da9a9cc50eea1f3
KARTHICKJK1208 <160461673+KARTHICKJK1208@users.noreply.github.com>
2025-05-05 12:59:23 +0530

    SearchBar Changed

--- movie-recommender-app/src/Pages/Components/SearchBar.js
+++ movie-recommender-app/src/Pages/Components/SearchBar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./styles/SearchBarStyles.css";
-import "./styles/SearchBarStyles.css";
 
 function SearchBar({ movies, placeholder }) {
     const navigate = useNavigate();
